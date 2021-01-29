fd288f02708c741914c4b25a7eac188cce9e60e3
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-01-29 13:00:25 +0100

    Added questions

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+//Question 1:
+
+
+//Question 2:
+
+
+//Question 3:
+
+
+//Question 4:
+
+
+//Question 5:
+
+
+//Question 6:
+
+
+//Question 7:
+
+
+//Question 8:
