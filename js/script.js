1db0fd69b3e919a67cb6d8c87acd8b5b490d8794
John HC <69842878+Purgatos81@users.noreply.github.com>
2021-01-29 15:10:45 +0100

    Done 7.
    Mangler 8

--- js/script.js
+++ js/script.js
@@ -12,25 +12,51 @@ const cats = [
     }
 ];
 
-//Question 1:
+//uestion 1:
+const Cat = {complain: function catFunct() {
+    console.log("MeoW!");
+}
+};
+
+Cat.complain();
+
+//console.log(Cat.complain());
+
 
 
 //Question 2:
+const Heading = document.querySelector("h3");
+
+Heading.innerHTML = "Updated heading";
 
 
 //Question 3:
-
+Heading.style.fontSize = "2em";
 
 //Question 4:
+Heading.classList.add("subheading");
 
+//console.log(Heading);
 
 //Question 5:
+const Paragraphs = document.querySelectorAll("p");
+for (let para of Paragraphs) {
+    para.style.color = "red";
+} 
 
 
 //Question 6:
+const ResultsContainer = document.querySelector("div.results");
 
+ResultsContainer.innerHTML = "<p>New paragraph</p>";
+ResultsContainer.style.backgroundColor = "yellow";
 
 //Question 7:
-
+function thisList(list) {
+    for(i = 0; i < list.length; i++) {
+        console.log(list[i].name);
+    }
+}
+thisList(cats);
 
 //Question 8:
